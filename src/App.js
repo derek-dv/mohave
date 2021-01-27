b8fc9b99b2c0a18fdbbecfc46bedc2c0f54b712e
Derek David <derekmiracledavid@gmail.com>
2021-01-27 16:00:59 +0100

    re-writing to react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <div className="app__body">
+        <Sidebar />
+        <Feed />
+      </div>
+    </div>
+  );
+}
+
+export default App;
